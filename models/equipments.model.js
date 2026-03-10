a7189b79f54bcd1ff22cf1b3a0e0e61759f389c3
Eslam Saeed <eslamsaeed298@gmail.com>
2026-03-10 04:58:00 +0200

    feat: add Equipment model with schema for name and description

---
+++ models/equipments.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const equipmentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    }
+}, { timestamps: true });
+
+const Equipment = mongoose.model('Equipment', equipmentSchema);
+
+export default Equipment;
